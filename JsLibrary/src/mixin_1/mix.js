f8487a8cd35bbfe586ea688979a8ee46541b6826
kojiy <kojiy@192.168.11.5>
2017-01-09 02:56:09 +0900

    サンプルコード

---
+++ JsLibrary/src/mixin_1/mix.js
@@ -0,0 +1,69 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017 koji-yamashita.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/**
+ *
+ * @param {type} base 継承元クラス
+ * @param {type} mixins mixin対象クラス
+ * @returns {nm$_mix.exports.mix} baseとmixinsの機能を併せ持つ匿名クラスを返却
+ */
+exports.mix = function (base, ...mixins) {
+    /**
+     * コピー先にコピー元のプロパティをコピーする。
+     * ただしコンストラクタ、プロトタイプはコピーしない。
+     *
+     * @param {type} target プロパティのコピー先オブジェクト
+     * @param {type} source プロパティのコピー元オブジェクト
+     * @returns {undefined} 返却なし
+     */
+    let copyProperties = function (target = {}, source = {}) {
+        const ownPropertyNames = Object.getOwnPropertyNames(source);
+        ownPropertyNames
+            .filter(key => !/(prototype|name|constructor)/.test(key))
+            .forEach(key => {
+                const desc = Object.getOwnPropertyDescriptor(source, key);
+                Object.defineProperty(target, key, desc);
+            })
+    }
+
+    //引数のクラスを親クラスとする返却用匿名クラスを準備
+    let retval = class extends base {
+
+        constructor(...args) {
+            super(...args);
+            //利用したパラメータは削除する
+            args = args.splice(base.length);
+
+            for (let i in mixins) {
+                copyProperties(retval.prototype, mixins[i].prototype);
+
+                let mixinobj = new mixins[i](...args);
+                copyProperties(this, mixinobj);
+                args = args.splice(mixin[i].length);
+            }
+        }
+    }
+
+    return retval;
+}
---
+++ JsLibrary/src/mixin_1/sample.js
@@ -0,0 +1,70 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017 kojiy.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+
+const mix =require('./mix.js');
+
+class Container {
+    constructor(width, height){
+        this._width = width;
+        this._height = height;
+    }
+
+    get width(){
+        return this._width;
+    }
+
+    get height(){
+        return this._height;
+    }
+};
+
+class Placeable extends A{
+    constructor(x, y){
+        super();
+        this._x = x;
+        this._y = y;
+    }
+
+    get x(){
+        return this._x;
+    }
+
+    get y(){
+        return this._y;
+    }
+};
+
+class Window extends mix.mix(Container, Placeable){
+   constructor(width, height, x, y){
+       super(width, height, x, y);
+   }
+};
+
+let w = new Window(640, 320, 10, 100);
+
+console.log('width = ' + w.width);
+console.log('height = ' + w.height);
+console.log('x = ' + w.x);
+console.log('y = ' + w.y);
